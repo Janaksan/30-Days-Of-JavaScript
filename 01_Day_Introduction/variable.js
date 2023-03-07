274ae3808f9d447d021a5fb0a9fd22c87f588acd
Janakshan <janaksan.jana@gmail.com>
2023-03-07 11:08:35 +0530

    Declare four variables with assigned values

--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -12,7 +12,7 @@ let isUndefined;
 //null
 const isNull = null;
 
-//output
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
 
 console.log([
   {
@@ -32,3 +32,28 @@ console.log([
     type: typeof isNull,
   },
 ]);
+
+// 6. Declare four variables without assigning values
+
+let test_1;
+let test_2;
+let test_3;
+let test_4;
+
+test_1 = "some";
+
+console.log(test_1, test_2, test_3, test_4);
+
+// 7.Declare four variables with assigned values
+
+let firstName = "Krish";
+let lastName = "Janakshan";
+let age = 30;
+let isMale = true;
+
+console.log({
+  firstName,
+  lastName,
+  age,
+  isMale,
+});
