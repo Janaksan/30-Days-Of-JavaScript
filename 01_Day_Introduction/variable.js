1f23952c053b142986424514269f11c8c1e47fa6
Janakshan <janaksan.jana@gmail.com>
2023-03-07 10:38:59 +0530

    4. declare variables and assign string, boolean, undefined and null data types

---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,34 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+// string
+const title = "Janakshan";
+
+// boolean
+const isTrue = false;
+
+//undefined
+let isUndefined;
+
+//null
+const isNull = null;
+
+//output
+
+console.log([
+  {
+    string: title,
+    type: typeof title,
+  },
+  {
+    string: isTrue,
+    type: typeof isTrue,
+  },
+  {
+    string: isUndefined,
+    type: typeof isUndefined,
+  },
+  {
+    string: isNull,
+    type: typeof isNull,
+  },
+]);
